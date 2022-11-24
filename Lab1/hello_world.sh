f16895c203f2370918e1054a1a75d45e296e6aeb
Ryan <tranryan13@gmail.com>
2022-11-24 14:34:20 -0600

    Lab1 with Menu

---
+++ Lab1/hello_world.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Bash Menu Script Example
+PS3="Select your option by entering in a number: "
+
+select option in Problem-1a Problem-1b Problem-1c Problem-2a Problem-2b Problem-3 Quit
+do
+    case $option in
+        "Problem-1a")
+            echo "Selected item $option $REPLY"
+            proc=$(nproc --all)
+            echo "Number of Cores on this machine: ${proc}"
+            echo "Enter in the number of processors n:"
+            read n
+            echo "Selected number of processors n:${n}"
+            mpirun -np $n ./a.out;;
+        "Problem-1b")
+           echo "Selected item $option $REPLY"
+           proc=$(nproc --all)
+           echo "Number of Cores on this machine: ${proc}"
+           echo "Enter in the number of processors n:"
+           read n
+           echo "Selected number of processors n:${n}"
+           mpirun -np $n ./a.out;;
+        "Problem-1c")
+           echo "Selected item $option $REPLY";;
+        "Problem-2a")
+            echo "Selected item $option $REPLY";;
+        "Problem-2b")
+           echo "Selected item $option $REPLY";;
+        "Problem-3")
+           echo "Selected item $option $REPLY";;
+        "Quit")
+           echo "We're done"
+           break;;
+        *)
+           echo "invalid option $REPLY";
+    esac
+done
+#Sources: https://askubuntu.com/questions/1705/how-can-i-create-a-select-menu-in-a-shell-script
+#Sources: https://www.baeldung.com/linux/shell-script-simple-select-menu#:~:text=We%20should%20use%20the%20select,of%20options%20preceded%20by%20numbers.&text=select%20repeatedly%20reads%20a%20number,NAME%20to%20the%20respective%20text.
+#mpirun -np 5 ./hello-world
---
+++ Lab1/readme.md
@@ -0,0 +1,6 @@
+<h1> Making Hello World.c </h1>
+make hello
+<h1>Running Hello World.c</h1>
+Run the batch script command below
+```./hello_world.sh ```
+
