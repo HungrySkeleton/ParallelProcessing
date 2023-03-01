e99b3180cb619f7449e24e75f19f58c4441058b5
Hungry Skeleton <Tranryan13@gmail.com>
2023-03-01 01:40:06 -0600

    Beginning WireWorld and Exploring QT for GUI

---
+++ WireWorld/WireWorld.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <mpi.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+
+using namespace std;
+
+void simulate();
+void rules();
+
+int main()
+{
+    int iterations = 50;
+    cv::Mat image;
+    // 'Hello World!' program 
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
+
+void simulate()
+{
+    //Tick
+    for(int i = 0; i < iterations; i++)
+    {
+        //Apply Rules
+        
+
+    }
+
+}
